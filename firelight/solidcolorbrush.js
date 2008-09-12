4e19a5e77a91f37c4558317add5ee23654f944a4
Chris Toshok <toshok@gmail.com>
2008-09-11 20:35:58 -0700

    set the color in the ctor, and add a visit method

--- firelight/solidcolorbrush.js
+++ firelight/solidcolorbrush.js
@@ -1,7 +1,15 @@
 function SolidColorBrush (color)
 {
     Brush.apply (this, []);
+
+    this.color = color;
 }
 
 SolidColorBrush.prototype = $.extend(new Brush(), {
+
+	visit: function (visitor) {
+	    visitor.visitSolidColorBrush (this);
+	}
 });
+
+DependencyProperties.register (SolidColorBrush, "Color");
