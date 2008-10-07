21cd6ee07aadbab98c37c6ad90baf2b6a7af828a
Chris Toshok <toshok@gmail.com>
2008-10-06 22:21:58 -0700

    make some methods actually private

--- firelight/xamltyperesolver.js
+++ firelight/xamltyperesolver.js
@@ -1,6 +1,6 @@
-// we simulate namespaces with associative arrays
-function TypesCtor () {
-    function registerNamespace (ns) {
+var Types = function () {
+  // we simulate namespaces with associative arrays
+  function registerNamespace (ns) {
 	var nss = ns.split('.');
 	var o = Types;
 	for (var i = 0; i < nss.length; i ++) {
@@ -11,80 +11,79 @@ function TypesCtor () {
 	    o = o[nss[i]];
 	}
 	return o;
-    };
+  };
 
-    return {
-	lookupNamespace: function (ns) {
-	    var nss = ns.split('.');
-	    var o = Types;
-	    for (var i = 0; i < nss.length; i ++) {
-		if (!(nss[i] in o))
-		    return null; // the namespace hasn't had any types
-			         // registered in it.
-		o = o[nss[i]];
-	    }
-	    return o;
-	},
+  return {
+    lookupNamespace: function (ns) {
+      var nss = ns.split('.');
+      var o = Types;
+      for (var i = 0; i < nss.length; i ++) {
+	if (!(nss[i] in o))
+	  return null; // the namespace hasn't had any types
+	// registered in it.
+	o = o[nss[i]];
+      }
+      return o;
+    },
 
-        registerType: function (ns, t) {
-	    var nsobj = registerNamespace (ns);
-	    var typeName = t.name;
-	    if (!typeName) throw new Error ("could not determine type name to register");
-	    nsobj[typeName] = t;
-	}
-    };
-};
+    registerType: function (ns, t) {
+      var nsobj = registerNamespace (ns);
+      var typeName = t.name;
+      if (!typeName) throw new Error ("could not determine type name to register");
+      nsobj[typeName] = t;
+    }
+  };
+} ();
 
-var Types = new TypesCtor();
-
-function XamlTypeResolverCtor () {
+var XamlTypeResolver = function () {
     var maps = {};
     return {
 	addNamespaceMap: function (xmlns, ns) {
-	    if (!maps[xmlns]) {
-		maps[xmlns] = [];
-	    }
-	    maps[xmlns].push (ns);
+	  if (!maps[xmlns]) {
+	    maps[xmlns] = [];
+	  }
+	  maps[xmlns].push (ns);
 	},
 
 	resolveQualifiedType: function (qualifiedTypeName) {
-//	    Trace.debug ("resolveQualifiedType (" + qualifiedTypeName + ")");
-	    var dot = qualifiedTypeName.lastIndexOf('.');
-	    if (dot == -1) throw new Error ("you must specify a fully qualified type name");
+//	  Trace.debug ("resolveQualifiedType (" + qualifiedTypeName + ")");
+	  var dot = qualifiedTypeName.lastIndexOf('.');
+	  if (dot == -1) throw new Error ("you must specify a fully qualified type name");
 
-	    var ns = qualifiedTypeName.substring (0, dot);
-	    var typeName = qualifiedTypeName.substring (dot+1);
+	  var ns = qualifiedTypeName.substring (0, dot);
+	  var typeName = qualifiedTypeName.substring (dot+1);
 
-	    var t = null;
-	    var nsobj = Types.lookupNamespace (ns);
-	    if (nsobj && typeName in nsobj) {
-		t = nsobj[typeName];
-	    }
+	  var t = null;
+	  var nsobj = Types.lookupNamespace (ns);
+	  if (nsobj && typeName in nsobj) {
+	    t = nsobj[typeName];
+	  }
 
-	    if (!t) throw new Error ("unable to resolve type '" + qualifiedTypeName + "'");
-	    return t;
+	  if (!t) throw new Error ("unable to resolve type '" + qualifiedTypeName + "'");
+	  return t;
 	},
 
 	resolveType: function (nodename, namespace) {
-	    var ns_list = maps[namespace];
-	    if (!ns_list) throw new Error ("unable to locate mapping for xmlns '" + namespace + "'");
+	  if (typeof (namespace) == "undefined")
+	    namespace = FirelightConsts.XAMLns;
 
-	    var t = null;
-	    for (var i = 0; i < ns_list.length; i ++) {
-		var ns = ns_list[i];
-		var nsobj = Types.lookupNamespace (ns);
-		if (nsobj && nodename in nsobj) {
-		    t = nsobj[nodename];
-		    break;
-		}
-	    }
-	    if (!t) throw new Error ("unable to resolve type '" + nodename + "' in xmlns '" + namespace + "'");
-	    return t;
+	  var ns_list = maps[namespace];
+	  if (!ns_list) throw new Error ("unable to locate mapping for xmlns '" + namespace + "'");
+
+	  var t = null;
+	  for (var i = 0; i < ns_list.length; i ++) {
+	    var ns = ns_list[i];
+	    var nsobj = Types.lookupNamespace (ns);
+	    if (nsobj && nodename in nsobj) {
+	      t = nsobj[nodename];
+		break;
+	      }
+	  }
+	  if (!t) throw new Error ("unable to resolve type '" + nodename + "' in xmlns '" + namespace + "'");
+	  return t;
 	}
     };
-};
-
-var XamlTypeResolver = new XamlTypeResolverCtor();
+} ();
 
 XamlTypeResolver.addNamespaceMap (FirelightConsts.XAMLns, "System.Windows");
 XamlTypeResolver.addNamespaceMap (FirelightConsts.XAMLns, "System.Windows.Controls");
